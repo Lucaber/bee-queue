f64a4bde1523e555c54954e9281152ce2b61ba95
Bond <bondz@users.noreply.github.com>
2018-07-17 09:08:39 +0100

    Add isRunning

--- index.d.ts
+++ index.d.ts
@@ -26,7 +26,9 @@ declare class BeeQueue<T> extends EventEmitter {
   on(ev: "job progress",  fn: (jobId: string, progress: number) => void): this;
 
   ready(): Promise<this>;
-  ready(cb: (err: Error | null) => void): this;
+  ready(cb?: (err: Error | null) => void): Promise<this>;
+
+  isRunning(): boolean;
 
   createJob(data: T): BeeQueue.Job<T>;
 
